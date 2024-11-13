f8786a96ee743e1c028b6b1f91828d0d398f1704
Rylie <rylie.stone2005@gmail.com>
2024-11-13 08:02:37 -0500

    added comment for future middleware with future adds

---
+++ api/get/get-middleware.js
@@ -0,0 +1,9 @@
+
+// use this file to make middle ware for use when the end points are being called 
+// use a async function with the following requirements (req, res, next)
+// the req is going to be the data sent and the res is to send responses back
+// call next() when you want it to go to the next step in the endpoint
+
+
+module.exports = {
+}
