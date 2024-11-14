bc45cdbd83b216cb50ff2457dfe3fec56dc7ad90
Rylie <rylie.stone2005@gmail.com>
2024-11-14 10:07:07 -0500

    finished github loop

--- api/get/get-model.js
+++ api/get/get-model.js
@@ -10,6 +10,34 @@ async function github(GithubUsername) {
     repos = [...res.data]
   }).catch(err => console.log(err))
   for (let i in repos) {
+    let project = repos[i]
+    projects.push({
+      id: project.id,
+      name: project.name,
+      html_url: project.html_url,
+      description: project.description,
+      homepage: project.homepage,
+      size: project.size,
+      stargazers_count: project.stargazers_count,
+      watchers_count: project.watchers_count,
+      language: project.language,
+      open_issues: project.open_issues,
+      open_issues_count: project.open_issues_count,
+      fork_count: project.fork_count,
+      forks: project.forks,
+      archived: project.archived,
+      disabled: project.disabled,
+      watchers: project.watchers,
+      license: {...project.license}
+    })
   }
   return GithubUsername
 }
+//   license: {
+//     key: 'mit',
+//     name: 'MIT License',
+//     spdx_id: 'MIT',
+//     url: 'https://api.github.com/licenses/mit',
+//     node_id: 'MDc6TGljZW5zZTEz'
+//   },
+//   watchers: 0,
