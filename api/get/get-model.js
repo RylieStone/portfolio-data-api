856c5436cc8badc30b6574c93b7472685a08cb03
Rylie <rylie.stone2005@gmail.com>
2024-11-14 09:50:03 -0500

    WIP github code

--- api/get/get-model.js
+++ api/get/get-model.js
@@ -1,8 +1,15 @@
 // DO NOT MAKE CHANGES TO THIS FILE
+const axios = require('axios')
 module.exports = {
   github,
 };
-function github(GithubUsername) {
-console.log(GithubUsername)
-return GithubUsername
+async function github(GithubUsername) {
+  let repos = []
+  let projects = []
+  await axios.get(`https://api.github.com/users/${GithubUsername}/repos`).then(res => {
+    repos = [...res.data]
+  }).catch(err => console.log(err))
+  for (let i in repos) {
+  }
+  return GithubUsername
 }
